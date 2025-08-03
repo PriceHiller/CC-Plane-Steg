462972acf4d259d81675eb04583189e842bd3afe
Price Hiller <price@price-hiller.com>
2025-08-02 20:53:27 -0500

    refactor: embed `bitmanip.py` in `__init__.py`

--- src/ccps/__init__.py
+++ src/ccps/__init__.py
@@ -6,7 +6,17 @@ import itertools
 import typing
 from typing import NamedTuple, Self, override
 
-from ccps.bitmanip import embed_bit, extract_bit
+
+def embed_bit(byte: int, position: int, bit: int) -> int:
+    if bit < 0 or bit > 1:
+        raise ValueError(f"Bit to embed MUST be a 1 or a 0, received '{bit}'")
+    mask = ~(1 << position)
+    num = byte & mask
+    return num | (bit << position)
+
+
+def extract_bit(byte: int, position: int) -> int:
+    return (byte >> position) & 1
 
 
 class ColorComponent(Enum):
